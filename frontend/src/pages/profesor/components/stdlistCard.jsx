c865b62a0126a754f49562f9ea703a819e000243
Barty <josip.bartulovic3@skole.hr>
2019-01-04 00:44:35 +0100

    Fixed ucenikDetalji closing bug

--- frontend/src/pages/profesor/components/stdlistCard.jsx
+++ frontend/src/pages/profesor/components/stdlistCard.jsx
@@ -1,48 +1,42 @@
-import React, { Component } from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import ContentCard from '../../../common/content-card/contentCard'
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import UcenikDetalji from './ucenikDetalji';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import ContentCard from "../../../common/content-card/contentCard";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import UcenikDetalji from "./ucenikDetalji";
 
 const styles = theme => ({
-    root: {
-        overflow: 'hidden',
-        width: '100%',
-    }
-})
+  root: {
+    overflow: "hidden",
+    width: "100%"
+  }
+});
 
 class stdlistCard extends Component {
-    handleClickOpen = () => {
-        this.setState({ open: true });
-    };
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
 
-    handleClose = () => {
-        this.setState({ open: false });
-    };
-    render() {
-        const { classes } = this.props
-        return (    
-            <ContentCard cardName='Moji razredi' className={classes.root}>
-               <List component="nav"className={classes.root}>
-                       <UcenikDetalji/>
-                     <Divider />
-                     <ListItem button>
-                       <ListItemText primary="4.F" />
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  render() {
+    const { classes } = this.props;
+    return (
+      <ContentCard cardName="Moji razredi" className={classes.root}>
+        <List component="nav" className={classes.root}>
+          <UcenikDetalji />
+          <Divider />
+          <ListItem button>
+            <ListItemText primary="4.F" />
+          </ListItem>
+          <Divider />
+        </List>
+      </ContentCard>
+    );
+  }
+}
 
-                     </ListItem>
-                     <Divider />
-                </List>
-
-             </ContentCard>
-            )
-        }
-    
-    }
-    
 export default withStyles(styles)(stdlistCard);
