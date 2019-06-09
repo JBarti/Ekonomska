d50a2402a2e19cd61666fd990f351d177bc03a50
JBarti <josip.bartulovic3@skole.hr>
2019-06-10 00:58:28 +0200

    :hammer:

--- frontend/src/actions/studentActions.jsx
+++ frontend/src/actions/studentActions.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const API_ENDPOINTG = "https://f-pismenost.herokuapp.com";
-const API_ENDPOINT = "http://0.0.0.0:3001";
+const API_ENDPOINT = "https://f-pismenost.herokuapp.com";
+const API_ENDPOINTG = "http://0.0.0.0:3001";
 
 export function loadStudent(email, password) {
   return {
